a7da42ea2edd978fcb6ac072b45b2ba50f2f6cca
Tomas Drozdik <drozdik.tomas@gmail.com>
2021-04-10 16:12:26 +0200

    Update permissions add gitignore

--- as2/Example-Py/client.py
+++ as2/Example-Py/client.py
@@ -7,13 +7,13 @@ from thrift.protocol import TMultiplexedProtocol
 from Example import Example
 
 # Connect to server by TCP socket
-socket = TSocket.TSocket('localhost', 5000)
+socket = TSocket.TSocket('lab.d3s.mff.cuni.cz', 5000)
 ## Use buffering
 transport = TTransport.TBufferedTransport(socket)
 ## Use a binary protocol to serialize data
 muxProtocol = TBinaryProtocol.TBinaryProtocol(transport)
 ## Use a multiplexed protocol to select a service by name
-protocol = TMultiplexedProtocol.TMultiplexedProtocol(muxProtocol, "Example")
+protocol = TMultiplexedProtocol.TMultiplexedProtocol(muxProtocol, "ActualExample")
 ## Proxy object
 client = Example.Client(protocol)
 ## Open the connection
